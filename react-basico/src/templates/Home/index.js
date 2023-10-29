b97201dd7dc0ac857f39826cb44975ab2599ef49
AnaJordao <acjpjvjp@gmail.com>
2023-10-29 16:23:33 -0300

    Carregando 10 posts por página

--- react-basico/src/templates/Home/index.js
+++ react-basico/src/templates/Home/index.js
@@ -1,9 +1,19 @@
 import { Component } from 'react';
+
 import './styles.css';
+
+import { Posts } from '../../components/Posts';
+import { loadPosts } from '../../utils/load-posts';
+import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
 export class Home extends Component {
   state = {
-    posts: []
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 10,
+    searchValue: ''
   }
 
   componentDidMount() {
@@ -11,36 +21,73 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+    const { page, postsPerPage } = this.state
 
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse])
-
-    const postsJson = await posts.json()
-    const photosJson = await photos.json()
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url }
+    const postsAndPhotos = await loadPosts()
+    this.setState({ 
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos
     })
+  }
 
-    this.setState({ posts: postsAndPhotos })
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state
+
+    const nextPage = page + postsPerPage
+    const nextPosts = allPosts.slice(nextPage, nextPage+postsPerPage)
+    posts.push(...nextPosts)
+
+    this.setState({ posts, page: nextPage })
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target
+    this.setState({ searchValue: value })
   }
 
   render() {
-    const { posts } = this.state
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state
+    const noMorePosts = page + postsPerPage >= allPosts.length
+
+    const filteredPosts = !!searchValue ?
+      allPosts.filter(post => {
+        return post.title.toLowerCase().includes(
+          searchValue.toLowerCase()
+        )
+      })
+      : posts
 
     return(
       <section className='container'>
-        <div className='posts'>
-          {posts.map(post => (
-            <div className='post'>
-              <img src={post.cover} alt={post.title}/>
-              <div key={post.id} className='post-content'>
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
-          ))}
+        <div className='search-container'>
+          {!!searchValue && (
+            <h1>Search value: {searchValue}</h1>
+          )}
+
+          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+        </div>
+        
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts}/>
+        )}
+
+        {filteredPosts.length === 0 && (
+          <p>Não existem posts :( </p>
+        )}
+
+        <div className='button-container'>
+          {!searchValue && (
+            <Button
+              text="Load more posts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     )
