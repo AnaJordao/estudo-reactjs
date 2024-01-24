46bb9d21fe6b6ff2bb619951370c9c1842732358
AnaJordao <acjpjvjp@gmail.com>
2024-01-24 17:47:11 -0300

    React Router Dom v6

---
+++ react-router-dom-v6/src/components/Post/index.tsx
@@ -0,0 +1,6 @@
+export const Post = () => {
+    return <div>
+        <h1>Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi temporibus nihil et voluptatum amet, est esse repellendus unde. Nisi tenetur corporis excepturi velit ratione numquam quae dignissimos eius laboriosam fugit.</p>
+    </div>
+}
